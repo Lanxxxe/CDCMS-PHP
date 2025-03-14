61838b56c23d256217658552b2faeb4dbeab1ca4
0x13 <dilucrag19@gmail.com>
2025-03-14 23:18:29 +0800

    Disabled error logging

--- teacher/ai_recommendation.php
+++ teacher/ai_recommendation.php
@@ -1,6 +1,6 @@
 <?php
 error_reporting(E_ALL);
-ini_set("display_errors", 1);
+ini_set("display_errors", 0);
 session_start();
 $pageTitle = "Teacher Dashboard";
 require_once '../config/database.php';
